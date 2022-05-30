f5971953534792aeaffe2eaa5b8c875bbca56121
Zoanna <ann.zozulya@gmail.com>
2022-05-30 18:27:55 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Anna Zozulia on 30.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
